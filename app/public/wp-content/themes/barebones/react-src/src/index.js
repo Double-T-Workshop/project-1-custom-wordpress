8d4ed6543cebf64ba5350630fbc1925e99cb5cc6
Citronskal <yiaswift13@gmail.com>
2024-11-07 15:43:27 +0100

    added new files

--- app/public/wp-content/themes/barebones/react-src/src/index.js
+++ app/public/wp-content/themes/barebones/react-src/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import Single from './Single'; // Ensure this path is correct
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/page' element={<Single />} />
+    </Routes>
+  </BrowserRouter>
 );
+
+serviceWorker.unregister();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ app/public/wp-content/themes/barebones/react-src/templates/Single.js
@@ -0,0 +1,8 @@
+export const Single = (props) => {
+
+    return (
+        <div className='Post'>
+            This is the page component 
+        </div>
+    )
+}
