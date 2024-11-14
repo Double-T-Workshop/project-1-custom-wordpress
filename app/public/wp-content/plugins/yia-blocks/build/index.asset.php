da055ea52157d9a31c6a8b6c46078f7973d4c24b
Citronskal <yiaswift13@gmail.com>
2024-11-14 14:05:39 +0100

    yia block -text block

---
+++ app/public/wp-content/plugins/yia-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1d4c3bf6d23f06c18445');
---
+++ app/public/wp-content/plugins/yia-blocks/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a70336121343ef0075a6');
