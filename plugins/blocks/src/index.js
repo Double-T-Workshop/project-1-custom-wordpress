b15ea99b73ffbb0e46d17cc8555a5643726df25d
ittsai <144024844+ittsai@users.noreply.github.com>
2024-11-19 13:39:31 +0100

    Create index.js

---
+++ plugins/blocks/src/index.js
@@ -0,0 +1,4 @@
+// src/index.js
+import "./blocks/block-one"; // update to your folder name
+import "./blocks/block-two"; // update to your folder name
+import "./blocks/block-three"; // update to your folder name
