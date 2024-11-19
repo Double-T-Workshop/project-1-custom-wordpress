cd0fa225bbef9fc2f733fec559427c7706ea4093
ittsai <144024844+ittsai@users.noreply.github.com>
2024-11-19 13:48:30 +0100

    Update index.js

--- plugins/blocks/src/index.js
+++ plugins/blocks/src/index.js
@@ -1,4 +1,4 @@
 // src/index.js
-import "./blocks/block-one"; // update to your folder name
-import "./blocks/block-two"; // update to your folder name
-import "./blocks/block-three"; // update to your folder name
+import "./custom-blocks/block-one"; // update to your folder name
+import "./custom-blocks/block-two"; // update to your folder name
+import "./custom-blocks/block-three"; // update to your folder name
