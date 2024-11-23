d137093f4bf88ae8f1c6bcc757c5953cac677647
Citronskal <yiaswift@gmail.com>
2024-11-23 15:14:14 +0100

    custom blocks

---
+++ public/wp-content/plugins/custom-blocks/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    "index.js": "./src/index.js", // Include all block entry points
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@wordpress/block-editor": path.resolve(
+        __dirname,
+        "node_modules/@wordpress/block-editor"
+      ),
+    },
+  },
+  externals: {
+    "@wordpress/blocks": ["wp", "blocks"],
+    "@wordpress/block-editor": ["wp", "blockEditor"],
+    "@wordpress/components": ["wp", "components"],
+    "@wordpress/element": ["wp", "element"],
+    "@wordpress/i18n": ["wp", "i18n"],
+  },
+};
