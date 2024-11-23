dc43f9d2587981b96aef78a7a0fc562a35203172
Citronskal <yiaswift@gmail.com>
2024-11-23 15:09:25 +0100

    theme installed

---
+++ public/wp-content/themes/react-theme/src/App.js
@@ -0,0 +1,32 @@
+import { Component } from '@wordpress/element';
+import Header from './Header';
+import Footer from './Footer';
+import Posts from './Posts';
+import Pages from './Pages';
+
+export default class App extends Component {
+    state = {
+        darkTheme: true,
+    };
+
+    toggleTheme = () => {
+        this.setState(prevState => ({
+            darkTheme: !prevState.darkTheme,
+        }));
+    };
+
+    render() {
+        const { darkTheme } = this.state;
+        return (
+            <div>
+                <Header darkTheme={darkTheme} toggleTheme={this.toggleTheme} />
+                <main style={{ padding: '20px', background: darkTheme ? '#282c34' : '#f5f5f5', color: darkTheme ? 'white' : 'black' }}>
+                    {/* Main content goes here */}
+                    <Posts />
+                    <Pages />
+                </main>
+                <Footer darkTheme={darkTheme} />
+            </div>
+        );
+    }
+}
